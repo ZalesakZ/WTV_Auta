2e4b32165fe041a110756fe8099398120710d12f
Zdeněk Zálešák <zalesakz@office365.zcu.cz>
2024-12-20 04:00:22 +0100

    správný doplnění databáze a vykreslování aut

--- auta/app/Http/Controllers/PageController.php
+++ auta/app/Http/Controllers/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Auto;
 use App\Models\Typ;
+use Exception;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -11,6 +12,10 @@ class PageController extends Controller
     public function ukazIndex()
     {
         $auta = Auto::all();
-        return view('welcome',["auuta" => $auta]);
+        return view('auta1', ["auuta" => $auta]);
+
+        $auta = Auto::all();
+        return view('carModel', ["auuta" => $auta]);
     }
+    
 }
--- auta/app/Models/Auto.php
+++ auta/app/Models/Auto.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Auto extends Model
 {
     protected $table = "auto";
-    protected $fillable = ["jmeno", "barva", "typ", "zavodnik", "pocet_poharu", "historie", "objeveni"];
+    protected $fillable = ["jmeno", "barva", "typ", "zavodnik", "pocet_poharu", "historie", "auta1", "auta2", "auta3", "serialy", "hry"];
 
     public function typ()
     {
