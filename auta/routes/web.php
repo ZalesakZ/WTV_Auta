ed448dee5b2980ae81f5cc0742b5269e47bb40ab
Zdeněk Zálešák <zdenda.zalesak@gmail.com>
2025-01-12 16:32:32 +0100

    tak zase vracím fixní ID zpět

--- auta/routes/web.php
+++ auta/routes/web.php
@@ -16,9 +16,9 @@ Route::get('/auta3', [PageController::class, 'ukazIndex3']) ->name("auta3.route"
 Route::get('/hry', [PageController::class, 'ukazIndex4']) ->name("hry.route");
 Route::get('/serialy', [PageController::class, 'ukazIndex5']) ->name("serialy.route");
 
-Route::get("/carModel/{fixni_ID}", function (int $id) {
+Route::get("/carModel/{fixni_ID}", function (int $fixni_ID) {
 
-    $poleVsechnyAuta = ModelVsechnyAuta::find($id);
+    $poleVsechnyAuta = ModelVsechnyAuta::find($fixni_ID);
 
     if($poleVsechnyAuta === null)
     {
